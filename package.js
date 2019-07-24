ee3eb6834538436e9138b998f243ab2ef136d4cd
Siggi <4411176+icellan@users.noreply.github.com>
2019-07-24 23:44:45 +0200

    Added package versions used by this package

--- package.js
+++ package.js
@@ -6,17 +6,17 @@ Package.describe({
 });
 
 Package.onUse(api => {
-  api.use('ecmascript');
-  api.use('accounts-base', ['client', 'server']);
+  api.use('ecmascript@0.12.4');
+  api.use('accounts-base@1.4.3', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
-  api.use('accounts-oauth', ['client', 'server']);
-  api.use('moneybutton-oauth');
-  api.imply('moneybutton-oauth');
+  api.use('accounts-oauth@1.1.16', ['client', 'server']);
+  api.use('icellan:moneybutton-oauth@1.0.0');
+  api.imply('icellan:moneybutton-oauth@1.0.0');
 
   api.use('http', ['client', 'server']);
 
-  api.use(['accounts-ui', 'moneybutton-config-ui'], ['client', 'server'], { weak: true });
+  api.use(['accounts-ui', 'icellan:moneybutton-config-ui'], ['client', 'server'], { weak: true });
   api.addFiles("notice.js");
 
   api.addFiles("moneybutton.js");
