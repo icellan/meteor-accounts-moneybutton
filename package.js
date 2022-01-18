50e761b1aaea60342fa7d65896298e7ad862ed15
Siggi <4411176+icellan@users.noreply.github.com>
2022-01-18 23:07:02 +0100

    Updates packages

--- package.js
+++ package.js
@@ -6,17 +6,17 @@ Package.describe({
 });
 
 Package.onUse(api => {
-  api.use('ecmascript');
-  api.use('accounts-base', ['client', 'server']);
+  api.use('ecmascript@0.16.1');
+  api.use('accounts-base@2.2.1', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
-  api.imply('accounts-base', ['client', 'server']);
-  api.use('accounts-oauth', ['client', 'server']);
+  api.imply('accounts-base@2.2.1', ['client', 'server']);
+  api.use('accounts-oauth@1.0.0', ['client', 'server']);
   api.use('icellan:moneybutton-oauth@2.0.0');
   api.imply('icellan:moneybutton-oauth@2.0.0');
 
-  api.use('http', ['client', 'server']);
+  api.use('http@2.0.0', ['client', 'server']);
 
-  api.use(['accounts-ui', 'icellan:moneybutton-config-ui'], ['client', 'server'], { weak: true });
+  api.use(['accounts-ui@1.4.2', 'icellan:moneybutton-config-ui@1.0.0'], ['client', 'server'], { weak: true });
   api.addFiles("notice.js");
 
   api.addFiles("moneybutton.js");
